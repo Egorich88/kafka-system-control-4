2667de7f6c24907f7726d518e095df2eb42535ab
Egorich88 <georgia-2005@mail.ru>
2026-05-25 14:52:28 +1000

    refactor(styles): extract shared variables and scrollbars

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import './styles/shared/variables.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
